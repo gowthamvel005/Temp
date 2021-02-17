9c3d9f54964792af9bc483aa168d40af4bbce26b
gowthamvel005 <gowtham.pashtek@gmail.com>
2021-02-17 13:49:50 +0530

    changes

--- public/activity.js
+++ public/activity.js
@@ -19,14 +19,9 @@ connection.on('initActivity', function(data) {
 	);
 	var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-    var s = document.getElementById("mytext");
-	document.getElementById('demo').value += s.value;
+  /*  var s = document.getElementById("mytext");
+	document.getElementById('demo').value += s.value;*/
 
-	var f = document.getElementById("password");
-	document.getElementById('demo').value += f.value;
-	
-	var t = document.getElementById("data");
-	document.getElementById('demo').value += t.value;
 	
 });
 
@@ -34,11 +29,7 @@ function myFunction() {
 	s = document.getElementById("mytext").value;
 	document.getElementById("demo").innerHTML = s;
 
-	f = document.getElementById("password").value;
-	document.getElementById("demo").innerHTML = f;
 
-	t = document.getElementById("data").value;
-	document.getElementById("demo").innerHTML = t;
   }
 
 connection.on('clickedNext', function() {
