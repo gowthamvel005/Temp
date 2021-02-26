601aab71b59c2a7cd2a2e13960527a5e9a3f5498
gowthamvel005 <gowtham.pashtek@gmail.com>
2021-02-26 12:09:03 +0530

    changes

--- public/activity.js
+++ public/activity.js
@@ -4,51 +4,47 @@ define([
     Postmonger
 ){
 
+	var connection = new Postmonger.Session();
+	var payload = {};
+	var authToken;
 
 
+	connection.trigger('ready');
 
+	connection.on('initActivity', function(data) {
+		if (data) {
+			payload = data;
+			console.log(payload);
+		}
+		var message;
+		var hasInArguments = Boolean(
+			payload['arguments'] &&
+			payload['arguments'].execute &&
+			payload['arguments'].execute.inArguments &&
+			payload['arguments'].execute.inArguments.length > 0
+		);
+		var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-var connection = new Postmonger.Session();
-var payload = {};
-var authToken;
+	// var s = document.getElementById("names");
+	//	document.getElementById('mytext').value +=  s.options[s.selectedIndex].value;
+		
+	});
 
+	function callme()
+	{
+		var first = document.getElementById('mytext').value;
+		var last = document.getElementById('password').value;
+	//	get the valus in the index.html
+		document.getElementById('demo').value = first +' '+ last ;
 
-connection.trigger('ready');
-
-connection.on('initActivity', function(data) {
-	if (data) {
-		payload = data;
-		console.log(payload);
 	}
-	var message;
-	var hasInArguments = Boolean(
-		payload['arguments'] &&
-		payload['arguments'].execute &&
-		payload['arguments'].execute.inArguments &&
-		payload['arguments'].execute.inArguments.length > 0
-	);
-	var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-   // var s = document.getElementById("names");
- //	document.getElementById('mytext').value +=  s.options[s.selectedIndex].value;
-	
-});
-
-function callme()
-{
-	var first = document.getElementById('mytext').value;
-	var last = document.getElementById('password').value;
-//	get the valus in the index.html
-	document.getElementById('demo').value = first +' '+ last ;
-
-}
-
-connection.on('clickedNext', function() {
-	payload.name = "Gowtham";
-    payload['arguments'].execute.inArguments = [{"message": first+''+ last}];
-    payload['metaData'].isConfigured = true;	
-	connection.trigger('updateActivity', payload);
-});
+	connection.on('clickedNext', function() {
+		payload.name = "Gowtham";
+		payload['arguments'].execute.inArguments = [{"message": first+''+ last}];
+		payload['metaData'].isConfigured = true;	
+		connection.trigger('updateActivity', payload);
+	});
 
 });
 
